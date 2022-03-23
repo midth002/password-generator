a7770c0355be5403a73ceb5c4e504e4d6f8ab568
Andrew Midthun <midth002@gmail.com>
2022-03-23 15:20:17 -0500

    added the logic of an if else statement for the password length. Testing it on the console log to see if I got the message I want from the input values

--- assets/script.js
+++ assets/script.js
@@ -14,11 +14,24 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // 1. Create a variable to save user input from propmpts
-var passwordLength = prompt("Set your password length. The length needs to be 8 or more charaters.");
-var specialCharacters = confirm("Would you like special characters?"); 
-var numericCharacters = confirm("Would you like numeric characters?"); 
-var lowercaseCharacters = confirm("Would you like to include lowercase characters?"); 
-var uppercaseCharacters = confirm("Would you like to include uppcase characters?"); 
+var passwordLengthString = prompt("Set your password length. The length needs to be 8 or more charaters.");
+//var specialCharacters = confirm("Would you like special characters?"); 
+//var numericCharacters = confirm("Would you like numeric characters?"); 
+//var lowercaseCharacters = confirm("Would you like to include lowercase characters?"); 
+//var uppercaseCharacters = confirm("Would you like to include uppcase characters?"); 
+
+	var passwordLengthNumber = parseInt(passwordLengthString);
+	console.log(passwordLengthNumber);
+
+
+	if (passwordLengthNumber >= 8) {
+		console.log("Thank you!");
+	} else if (passwordLengthNumber < 8) {
+		console.log("You need more than 8 characters");
+	} else {
+		console.log("Please enter a numeric value");
+		
+	}
 
 
 
