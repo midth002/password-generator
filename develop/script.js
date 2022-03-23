3ca8ea9e61635213a2b79783e11eb811d7bcadc1
Andrew Midthun <midth002@gmail.com>
2022-03-23 14:40:30 -0500

    added pseudo code to the js file

--- develop/script.js
+++ develop/script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// 1. Create a variable to save user input from propmpts
+	// passwordLength (string) string into integer
+	// specialCharacters boolean 
+	// numericCharacters boolean 
+	// lowercaseCharacters boolean
+	// uppercaseCharacters boolean
+	// alert, confirm, prompt
+	// Use conditionals to validate inputs (password length (must be 8 or more characters, user enters no inputs)
+	// 
+//2. Create arrays for each type of character
+	//specialCharacters array, uppercase array, lowercase array, numeric
+//3. use a randomizer function to select characters from each array
+//4. generatePassword function -- grab values out of the arrays from the user input to generate a random password
+//5. Create empty array that represents the final password
+// 6. convert the function into a string and show the password to the user
