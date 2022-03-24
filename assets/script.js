f38fe1ec80be9c8f0b87f45a93ecd4a209cf798f
Andrew Midthun <midth002@gmail.com>
2022-03-24 09:57:41 -0500

    Added comments to each section of functions and to the global variables on what they are doing to generate the user's password. Added the finalArray to the beginning of the generatePassword() function so that it will empty the array once the user wants to generate a different password.

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1. Create a variable to save user input from propmpts
+// Create a variable to save user input from propmpts
 
 
 var specialInput = true; 
@@ -23,26 +23,34 @@ var uppercaseInput = true;
 var passwordLengthString;
 var passwordLengthNumber = 0;
 
+// Create arrays for each type of character
+	//specialCharacters array, uppercase array, lowercase array, numeric
+
 var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowercaseArray = "abcdefghijklmnopqrstuvwxyz".split("");
 var uppercaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var finalArray = [];
+var finalArray;
 
 
 
-
+	// Once user clicks on the generate password button, this function starts checking what the user wants in their password.
 	function generatePassword() { 
-			password = ""; 
+			
+		finalArray = [];
+			finalPassword = ""; 
+			// passwordLength (string) string into integer
 			passwordLengthString = prompt("Set your password length. The length needs to be 8 or more charaters.");
 			passwordLengthNumber = parseInt(passwordLengthString);
 			checkPasswordLength();
 			getUsersCharacters();
 			randomizer();
-			return password;
 
+			//convert the function into a string and show the password to the user
+			return finalPassword;
 		}
 		
+	// Checking if the password is between 8 to 128 from the users input. 
 	function checkPasswordLength() {
 		if (passwordLengthNumber >= 8 && passwordLengthNumber <= 128) {
 			return console.log(passwordLengthNumber);
@@ -57,32 +65,39 @@ var finalArray = [];
 			}
 		
 			else {
-				alert("Please set your password length. The length needs to be 8 or more charaters.");
+				alert("Please set your password length in a numeric value. The length needs to be between 8 to 128 characters.");
 				return generatePassword();
 			}
 	}
 
+	//4. getUsersCharacters -- grab values out of the arrays from the user input to generate a random password
 	function getUsersCharacters() {
+
+		// Special Characters type
 		specialInput = confirm("Would you like special characters?"); 
 		if (specialInput) {
-			// finalArray.unshift(specialArray);
 			finalArray = finalArray.concat(specialArray);
 		}
+
+		// Numeric Characters type
 		numericInput = confirm("Would you like numeric characters?"); 
 		if (numericInput) {
 			finalArray = finalArray.concat(numericArray);
 		} 
 		
+		// Lowercase Characters type
 		lowercaseInput = confirm("Would you like to include lowercase characters?"); 
 		if (lowercaseInput) {
 			finalArray = finalArray.concat(lowercaseArray);
 		}
-			
+		
+		// Uppercase Characters type 
 		uppercaseInput = confirm("Would you like to include uppcase characters?"); 
 		if (uppercaseInput) {
 			finalArray = finalArray.concat(uppercaseArray);
 		}
 
+		// If user didn't select any characters type, send an alert and have the user pick again.
 		if (!specialInput && !numericInput && !lowercaseInput && !uppercaseInput)
 			{
 				 alert("You must select at least one of the characters should be selected for your password.");
@@ -91,8 +106,9 @@ var finalArray = [];
 	}
 
 
-		
-
+	
+	//3. use a randomizer function to select characters from each array
+	// Using Math.Random to randomize the finalArray. Using a for loop against the number the user wants in the password.
 	function randomizer() {
 		
 		for (i = 0; i < passwordLengthNumber; i++) {
@@ -105,19 +121,6 @@ var finalArray = [];
 
 	
 
-	
 
-	// passwordLength (string) string into integer
-	// specialCharacters boolean 
-	// numericCharacters boolean 
-	// lowercaseCharacters boolean
-	// uppercaseCharacters boolean
-	// alert, confirm, prompt
-	// Use conditionals to validate inputs (password length (must be 8 or more characters, user enters no inputs)
-	// 
-//2. Create arrays for each type of character
-	//specialCharacters array, uppercase array, lowercase array, numeric
-//3. use a randomizer function to select characters from each array
-//4. generatePassword function -- grab values out of the arrays from the user input to generate a random password
-//5. Create empty array that represents the final password
+
 // 6. convert the function into a string and show the password to the user
